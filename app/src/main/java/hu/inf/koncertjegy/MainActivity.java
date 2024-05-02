4636d345c913f831c386dff8ab326201334f85cd
Gyuris Martin <h260480@stud.u-szeged.hu>
2024-05-02 22:52:09 +0200

    login saves input data for register + back button now works

--- app/src/main/java/hu/inf/koncertjegy/MainActivity.java
+++ app/src/main/java/hu/inf/koncertjegy/MainActivity.java
@@ -1,6 +1,7 @@
 package hu.inf.koncertjegy;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
+    private static final String PREFS_KEY = MainActivity.class.getPackage().toString();
+    private static final int SECRET_KEY = 99;
+    EditText userName;
+    EditText password;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +32,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        userName = findViewById(R.id.editTextUserName);
+        password = findViewById(R.id.editTextPassword);
+
+        preferences = getSharedPreferences(PREFS_KEY, MODE_PRIVATE);
+
     }
 
     public void login(View view) {
-        EditText userName = findViewById(R.id.editTextUserName);
-        EditText password = findViewById(R.id.editTextPassword);
 
         String userNameStr = userName.getText().toString();
         String passwordStr = password.getText().toString();
@@ -39,8 +50,42 @@ public class MainActivity extends AppCompatActivity {
 
     public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
-        intent.putExtra("SECRET_KEY", 99);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
 
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userName.getText().toString());
+        editor.putString("password", password.getText().toString());
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
--- app/src/main/java/hu/inf/koncertjegy/RegisterActivity.java
+++ app/src/main/java/hu/inf/koncertjegy/RegisterActivity.java
@@ -1,6 +1,10 @@
 package hu.inf.koncertjegy;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,25 +13,105 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = RegisterActivity.class.getName();
+    private static final String PREFS_KEY = MainActivity.class.getPackage().toString();
+    EditText userNameEditText;
+    EditText emailEditText;
+    EditText passwordEditText;
+    EditText passwordAgainEditText;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-
-//        Bundle bundle = getIntent().getExtras();
-//        bundle.getInt("SECRET_KEY");
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        
         int secretKey = getIntent().getIntExtra("SECRET_KEY", 0);
 
         if (secretKey != 99) {
             finish();
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        userNameEditText = findViewById(R.id.userNameEditText);
+        emailEditText = findViewById(R.id.userEmailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordAgainEditText = findViewById(R.id.passwordAgainEditText);
+
+        preferences = getSharedPreferences(PREFS_KEY, MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        userNameEditText.setText(userName);
+        passwordEditText.setText(password);
+        passwordAgainEditText.setText(password);
+
     }
+
+    public void register(View view) {
+        String userName = userNameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String passwordAgain = passwordAgainEditText.getText().toString();
+
+        if (userName.isEmpty() || email.isEmpty() || password.isEmpty() || passwordAgain.isEmpty()) {
+            Log.e(TAG, "All fields are required!");
+            return;
+        }
+
+        if (!password.equals(passwordAgain)) {
+            Log.e(TAG, "Passwords do not match!");
+            return;
+        }
+
+        Log.i(TAG, "User name: " + userName + ", Email: " + email + ", Password: " + password + ", Password again: " + passwordAgain);
+
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        userNameEditText.setText(userName);
+        passwordEditText.setText(password);
+        passwordAgainEditText.setText(password);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
 }
